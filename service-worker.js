7e61b36356bc43fbd09bb071310ee21160ca15b9
SJH <shenjunhong@meizu.com>
2019-10-29 19:23:43 +0800

    增加了pwa，事例

--- service-worker.js
+++ service-worker.js
@@ -9,12 +9,37 @@ self.addEventListener('install', event => {
   )
 })
 
+self.addEventListener('install', function (event) {
+  event.waitUntil(self.skipWaiting()) //激活当前的worker，丢弃旧worker
+})
+//self.clients.claim（）
+
+
+self.addEventListener('active', function () {
+  event.waitUntil(self.clients.clamin)
+})
 self.addEventListener('fetch', (event) => {
   // if (/\.jpg$/.test(event.request.url)) {
   //   event.respondWith(fetch('/images/images.jpg'))
   // }
+
+  if (/\.jpg$/.test(event.request.url)) {
+    event.respondWith(
+      new Response('hello', {
+        headers: {
+          'Content-Type': 'text/html'
+        }
+      })
+    )
+  }
+
+
   event.respondWith(
-    caches.match(event.request,{ignoreSearch:true}).then(function (response) {
+    caches.match(event.request, {
+      ignoreSearch: true,
+      // ignoreMethod; true, //忽略请求的方法
+      // ignoreVary: true //忽略已缓存响应中的vary响应头
+    }).then(function (response) {
       if (response) {
         return response
       }
@@ -39,7 +64,26 @@ self.addEventListener('fetch', (event) => {
           return response;
         })
     })
-
-
   )
+
+  self.addEventListener('fetch', (event) => {
+    if (/\.jpg$ | .png$/.test(event.request.url)) {
+      var supportsWebp = false;
+      if (event.request.headers.has('accept')) {
+        supportsWebp = event.request.headers.get('accept').includes('webp');
+      }
+
+      if(supportsWebp){
+        var req = event.request.clone();
+        var returnUrl = req.url.substr(0, req.url.lastIndexOf('.')) + '.webp';
+
+        event.respondWith(
+          fetch(returnUrl,{
+            mode: "no-cors"
+          })
+        )
+      }
+    }
+  })
+
 })
