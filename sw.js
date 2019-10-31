f7459bf3d1b24a970ad7e6d92fd606e15c4632be
SJH <shenjunhong@meizu.com>
2019-10-31 10:46:01 +0800

    增加了server.js推送后台

--- sw.js
+++ sw.js
@@ -36,5 +36,44 @@ self.addEventListener('fetch',(event)=>{
     event.respondWith(new Response('123'))
 })
 
+/**
+ * 阻止添加到主屏幕
+ */
+window.addEventListener('beforeinstallprompt', function(e){
+  e.preventDefault();
+  return false;
+})
+
+/**
+ * 追踪用户是否添加到主屏幕
+ */
+window.addEventListener('beforeinstallprompt', function(e){
+  e.userChoice.then(function(result){
+    console.log(result.outcome);
+    if(result.outcome){
+
+    } else{
+
+    }
+  })
+})
+
+/**
+ * 推迟的原理是去掉原生的这个事件，转为我们自定义的btn事件
+ */
+
+
+/**
+ * 推送的订阅
+ */
+
+
+
+
+ /**
+  * 取消订阅通知
+  */
+
+
 
 const { Generat  }  = require('workbox-webpack-plugin')
